57756ccd2b5301401a53a06853a0f249802e6ef9
Leonov Dmitryiy <d.leonov@x5.ru>
2023-03-22 22:52:15 +0300

    basic + db user

--- src/main/java/com/example/security/configuration/SecurityConfig.java
+++ src/main/java/com/example/security/configuration/SecurityConfig.java
@@ -6,11 +6,18 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests().anyRequest().authenticated()
